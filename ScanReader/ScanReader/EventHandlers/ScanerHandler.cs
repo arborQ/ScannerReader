ed1081d1746811557d23b15219c2445ceab105ec
arbor <arbor@o2.pl>
2016-10-02 13:17:54 +0200

    978

---
+++ ScanReader/ScanReader/EventHandlers/ScanerHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ScanReader.EventHandlers
+{
+    public interface IScanerHandler
+    {
+        void OnInputEnter(object sender, KeyEventArgs e);
+    }
+
+    public class ScanerHandler : IScanerHandler
+    {
+        public ScanerHandler()
+        {
+            //uiElement.OnKeyDown += (e) => { }
+        }
+
+        public void OnInputEnter(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
--- ScanReader/ScanReader/MainWindow.xaml.cs
+++ ScanReader/ScanReader/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ScanReader.EventHandlers;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +21,49 @@ namespace ScanReader
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public ScanerHandler Scaner;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _code;
+        public string Code
+        {
+            get
+            {
+                return _code;
+            }
+            set
+            {
+                OnPropertyChanged();
+                _code = value;
+            }
+        }
+
         public MainWindow()
         {
+            Scaner = new ScanerHandler();
             InitializeComponent();
+            this.DataContext = this;
+            this.Code = "test";
+        }
+
+        private void OnWindowKeyDownEvent(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                Close();
+                return;
+            }
+            Scaner.OnInputEnter(sender, e);
+        }
+
+        //[NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
